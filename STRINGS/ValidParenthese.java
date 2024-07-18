487a54bc04a8791aef093857ed90cb1f21bf65b4
subhadipsingha <107751378+SubhadipSingha@users.noreply.github.com>
2024-07-18 09:54:15 +0530

    ADDED one PROGRAM TO THE STRING

---
+++ STRINGS/ValidParenthese.java
@@ -0,0 +1,31 @@
+package STRINGS;
+
+public class ValidParenthese {
+    
+        public static boolean isValid(String s) {
+            int n = s.length();
+            boolean Valid = false;
+            for(int i = 0;i<n-1;i++){
+                char c1 = s.charAt(i);
+                char c2 = s.charAt(i+1);
+                if(c1=='(' && c2==')'){
+                    Valid = true; 
+                }else if(c1=='{' && c2=='}'){
+                    Valid = true; 
+                }else if(c1=='[' && c2==']'){
+                    Valid = true; 
+                }else{
+                    Valid = false;
+                }
+            }
+            return Valid & n%2==0;
+        }
+        public static void main(String[] args) {
+            System.out.println(isValid("()")); // True
+            System.out.println(isValid("()[]{}")); // True
+            System.out.println(isValid("([)]")); // False
+            System.out.println(isValid("{")); // False (odd length)
+          }
+
+}
+
